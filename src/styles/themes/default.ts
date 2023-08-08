135570927753bc4b9e8eefade51c0d39af56b9f0
Nadi Duno <nadiduno@gmail.com>
2023-08-08 00:51:43 -0300

    Setup Project React

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,11 @@
+export const defaultTheme = {
+    white: '#fff',
+    black: '#000',
+
+    'color-3': '#1b1b1b',
+    'color-4': '#5A4B29',
+    'color-5': '#FF6914',
+    'color-6': '#D9D9D9',
+    'color-7': '#8C8A88',
+    'color-8': '#6D6D6D',
+} as const 
