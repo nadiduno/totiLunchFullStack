434a2d40e579740eaa62b65df84d5d0f1b77e185
Nadi Duno <nadiduno@gmail.com>
2023-08-12 18:54:18 -0300

    Card Price

--- src/components/CardPrice/index.tsx
+++ src/components/CardPrice/index.tsx
@@ -1,6 +1,33 @@
+import { Ball, BordBall, Details1, Option, OptionPay, OptionPay2, OrderDetails } from "./styles";
+
 export function CardPrice() {
     return (
-        <>
-        </>
+        <OrderDetails>
+            <Details1>
+                <p>Dividir a conta?</p>
+                <BordBall>
+                    <Ball></Ball>
+                    <Option>Não</Option>
+                </BordBall>
+            </Details1>
+            <Details1>
+                <p>Forma de Pagamento</p>
+                <OptionPay>
+                    <OptionPay2>
+                        <input type="checkbox" id="pix" name="pix" checked />
+                        <p>pix</p>
+                    </OptionPay2>
+                    <OptionPay2>
+                        <input type="checkbox" id="money" name="money" />
+                        <p>Dinheiro</p>
+                    </OptionPay2>
+                    <OptionPay2>
+                        <input type="checkbox" id="card" name="card" />
+                        <p>Cartâo</p>
+                    </OptionPay2>
+                </OptionPay>
+            </Details1>
+        </OrderDetails>
+
     )
 }
--- src/components/CardPrice/styles.tsx
+++ src/components/CardPrice/styles.tsx
@@ -1,5 +1,50 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.header`   
-    
+export const OrderDetails = styled.div`   
+    width: 90%;
+    height: 11.563rem;
+    border: 3px solid ${props => props.theme['color-4']};
+    padding: 1.375rem;    
+`;
+
+export const Details1 = styled.div`   
+    padding: .5rem;
+    margin-bottom: 2.125rem;
+`;
+
+export const BordBall = styled.div`   
+    width: 52px;
+    height: 20px;
+    border-radius: 20px;
+    border: 1px solid ${props => props.theme['white']};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 2px;
+    margin-top: .5rem;
+`;
+
+export const OptionPay = styled.div`   
+    display: inline-flex;
+    gap: 2rem;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: .5rem;
+`;
+
+export const OptionPay2 = styled.div`   
+    display: flex;
+    gap: .25rem;
+`;
+
+export const Ball = styled.div`   
+    width: 18px;
+    height: 18px;
+    border-radius: 100%;
+    background: ${props => props.theme['white']};
+`;
+
+export const Option = styled.div`   
+    font-size: .75rem;
+    color: ${props => props.theme['color-8']};
 `;
